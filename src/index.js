16cc8a254c438887b1282e3f9a10e5c5bd8cbcba
Polina <48429639+PolzaR@users.noreply.github.com>
2020-10-02 22:55:18 +0300

    feat: update solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let result;
+    let arr = n.toString().split('');
+    if(arr[0] === '-') {
+        result = arr.slice(1,arr.length).reverse().join('');
+    } else {
+        result = arr.reverse().join('')
+    }
+    return result;
 }
